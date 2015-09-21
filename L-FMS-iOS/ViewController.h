fa8f85149bcbd22240e41d583ba786e36db8f8a3
RuiZhang <tigercat.ruizhang@gmail.com>
2015-09-21 21:56:19 +0800

    Initial Commit

---
+++ L-FMS-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  L-FMS-iOS
+//
+//  Created by 虎猫儿 on 15/9/21.
+//  Copyright (c) 2015年 虎猫儿. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ L-FMS-iOSTests/L_FMS_iOSTests.m
@@ -0,0 +1,40 @@
+//
+//  L_FMS_iOSTests.m
+//  L-FMS-iOSTests
+//
+//  Created by 虎猫儿 on 15/9/21.
+//  Copyright (c) 2015年 虎猫儿. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface L_FMS_iOSTests : XCTestCase
+
+@end
+
+@implementation L_FMS_iOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
